3bff42eaea2ee57fd45843a801023541278d63a9
Michael Hirshleifer <111mth@caltech.edu>
2013-05-26 23:45:05 +0000

    Import news and annotate

--- plots.py
+++ plots.py
@@ -8,15 +8,30 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
+import datetime
 
 
 returns = matplotlib.mlab.csv2rec('./returns_calculated.csv',delimiter=b',')
+news = matplotlib.mlab.csv2rec('./news.csv',delimiter=b',')
+
 
 #from IPython import embed; embed()
 
 x = returns['date']
 y = returns['yhoo']
 fig = plt.figure()
-fig.add_subplot(111).plot(x, y)
+ax = fig.add_subplot(111)
+
+ax.plot(x, y)
+
+print('Footnotes (headline text):')
+for i, (time, headline) in enumerate(news):
+	print('[{0}] {1} "{2}"'.format(i, time, headline))
+	ax.annotate('[{0}]'.format(i), xy=(time, 0),
+		xytext=(time, 0.1),
+		arrowprops={'facecolor': 'black', 'shrink': 0.05, 'width': 0},
+		horizontalalignment='center', verticalalignment='top',
+	)
+
 plt.show()
 
