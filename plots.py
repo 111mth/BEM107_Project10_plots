f6b70c3ee1650fed2fdb14258e285e057ca37357
Michael Hirshleifer <111mth@caltech.edu>
2013-05-27 00:08:40 +0000

    Format annotations

--- plots.py
+++ plots.py
@@ -8,29 +8,38 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-import datetime
+from datetime import datetime
 
 
 returns = matplotlib.mlab.csv2rec('./returns_calculated.csv',delimiter=b',')
 news = matplotlib.mlab.csv2rec('./news.csv',delimiter=b',')
 
 
-#from IPython import embed; embed()
 
 x = returns['date']
 y = returns['yhoo']
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
+ax.set_xlim((datetime(2008, 01, 01), datetime(2008, 03, 01)))
+
 ax.plot(x, y)
+#from IPython import embed; embed()
 
 print('Footnotes (headline text):')
+(ymin, ymax) = ax.get_ybound()
 for i, (time, headline) in enumerate(news):
 	print('[{0}] {1} "{2}"'.format(i, time, headline))
-	ax.annotate('[{0}]'.format(i), xy=(time, 0),
-		xytext=(time, 0.1),
-		arrowprops={'facecolor': 'black', 'shrink': 0.05, 'width': 0},
-		horizontalalignment='center', verticalalignment='top',
+	ax.annotate('[{0}]'.format(i),
+		xy=(time, ymin),
+		xytext=(time, ymax + 0.02 * (ymax - ymin) * (i % 5)),
+		arrowprops={
+			'arrowstyle': '-', #'shrink': 0.05,
+			#'width': 0,
+			#'linestyle': 'dotted',
+			'edgecolor': 'black', 'alpha': 0.2
+		},
+		horizontalalignment='center', verticalalignment='bottom',
 	)
 
 plt.show()
