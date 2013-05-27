f7ea4789f4b3ce6f3ac2edd3b3cd985bc68d7cbd
Michael Hirshleifer <111mth@caltech.edu>
2013-05-27 01:06:18 +0000

    reorganize code

--- plots.py
+++ plots.py
@@ -14,33 +14,39 @@ from datetime import datetime
 returns = matplotlib.mlab.csv2rec('./returns_calculated.csv',delimiter=b',')
 news = matplotlib.mlab.csv2rec('./news.csv',delimiter=b',')
 
-
-
-x = returns['date']
-y = returns['yhoo']
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-ax.set_xlim((datetime(2008, 01, 01), datetime(2008, 03, 01)))
-
-ax.plot(x, y)
-#from IPython import embed; embed()
-
 print('Footnotes (headline text):')
-(ymin, ymax) = ax.get_ybound()
 for i, (time, headline) in enumerate(news):
 	print('[{0}] {1} "{2}"'.format(i, time, headline))
-	ax.annotate('[{0}]'.format(i),
-		xy=(time, ymin),
-		xytext=(time, ymax + 0.02 * (ymax - ymin) * (i % 5)),
-		arrowprops={
-			'arrowstyle': '-', #'shrink': 0.05,
-			#'width': 0,
-			#'linestyle': 'dotted',
-			'edgecolor': 'black', 'alpha': 0.2
-		},
-		horizontalalignment='center', verticalalignment='bottom',
-	)
+
+def annotate_news(axis):
+	'Annotates the plot axis with news events.'
+	(ymin, ymax) = axis.get_ybound()
+	for i, (time, headline) in enumerate(news):
+		axis.annotate('[{0}]'.format(i),
+			xy=(time, ymin),
+			xytext=(time, ymax + 0.02 * (ymax - ymin) * (i % 5)),
+			arrowprops={'arrowstyle': '-', 'edgecolor': 'black', 'alpha': 0.2,},
+			horizontalalignment='center', verticalalignment='bottom',
+		)	
+
+#ax.set_xlim((datetime(2008, 01, 01), datetime(2008, 03, 01)))
+
+call = lambda f: f()
+
+@call
+def figure2a():
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+	ax.plot(returns['date'], returns['cumexcret_yhoo'])
+	ax.plot(returns['date'], returns['cumexcret_msft'])
+	annotate_news(ax)
+	fig.tight_layout()
+	return fig
+
+
+#from IPython import embed; embed()
+
+
 
 plt.show()
 
