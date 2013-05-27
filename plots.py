1ee8c04fbc3e0fca334b0e233cadb1ef4f18b2f2
Michael Hirshleifer <111mth@caltech.edu>
2013-05-27 04:14:38 +0000

    Use a second axis for news annotations

--- plots.py
+++ plots.py
@@ -18,16 +18,13 @@ print('Footnotes (headline text):')
 for i, (time, headline) in enumerate(news):
 	print('[{0}] {1} "{2}"'.format(i, time, headline))
 
-def annotate_news(axis):
-	'Annotates the plot axis with news events.'
-	(ymin, ymax) = axis.get_ybound()
-	for i, (time, headline) in enumerate(news):
-		axis.annotate('[{0}]'.format(i),
-			xy=(time, ymin),
-			xytext=(time, ymax + 0.02 * (ymax - ymin) * (i % 5)),
-			arrowprops={'arrowstyle': '-', 'edgecolor': 'black', 'alpha': 0.2,},
-			horizontalalignment='center', verticalalignment='bottom',
-		)	
+def annotate_news(axes):
+	'Annotates the plot axes with news events.'
+	news_axes = axes.twiny()
+	news_axes.set_xlim(axes.get_xlim())
+	news_axes.set_xticks(news['date'])
+	news_axes.set_xticklabels(map('[{0}]'.format,xrange(len(news))))
+	news_axes.xaxis.grid(True, linestyle='-', linewidth=1, color='black', alpha=0.2)
 
 #ax.set_xlim((datetime(2008, 01, 01), datetime(2008, 03, 01)))
 
@@ -36,13 +33,16 @@ call = lambda f: f()
 @call
 def figure2a():
 	fig = plt.figure()
-	ax = fig.add_subplot(1, 1, 1)
-	ax.plot(returns['date'], returns['cumexcret_yhoo'])
-	ax.plot(returns['date'], returns['cumexcret_msft'])
-	annotate_news(ax)
+	axes = fig.add_subplot(1, 1, 1)
+	axes.plot(returns['date'], returns['cumexcret_yhoo'])
+	axes.plot(returns['date'], returns['cumexcret_msft'])
+	annotate_news(axes)
 	fig.tight_layout()
 	return fig
 
+	#plt.ion()
+	#plt.show()
+	#from IPython import embed; embed()
 
 #from IPython import embed; embed()
 
